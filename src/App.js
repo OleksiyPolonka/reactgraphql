9552ecb8fd00a2da3992577e07ef452a5ff58046
oleksii.polonka <olpolonka@car-fluent.com>
2024-02-25 14:28:28 +0200

    working with graphql data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import github from "./db";
+import githubQuery from "./Query";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(github.baseURL, {
+          method: "POST",
+          headers: github.headers,
+          body: JSON.stringify(githubQuery),
+        });
+        const data = await res.json();
+
+        setUserName(data.data?.viewer?.name ?? "");
+      } catch (e) {
+        console.log("silent error handler");
+      }
+    })();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"> Repos</i>
+        <p>User name: {userName}</p>
+      </h1>
     </div>
   );
 }
